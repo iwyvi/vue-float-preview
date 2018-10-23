acfd86a9bb793e1dfc857592689f82de26419523
IwYvI <iwyvi@outlook.com>
2018-10-23 20:55:20 +0800

    doc: update example

--- examples/src/app.vue
+++ examples/src/app.vue
@@ -4,20 +4,17 @@
       class="vpp"
       :src="imageSrc"
       :content-style="`border-radius: 20px;box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.3);`"
-      :scale="true"
-      :track="true"
     ></float-preview>
     <float-preview
       class="vpp"
       :content-style="`border-radius: 20px;box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.3);`"
       :scale="false"
-      :track="true"
       :width="400"
-      :height="400"
+      :height="300"
     >
-      <h1 @click="change">test</h1>
+      <h1>vue-float-preview</h1>
       <template slot="preview">
-        <h2>test</h2>
+        <h2>Hello World!</h2>
       </template>
     </float-preview>
   </div>
@@ -28,15 +25,7 @@ import { Component, Vue } from 'vue-property-decorator';
 
 @Component
 export default class App extends Vue {
-  imageSrc =
-    'http://img.zcool.cn/community/0125fd5770dfa50000018c1b486f15.jpg@1280w_1l_2o_100sh.jpg';
-
-  change() {
-    setTimeout(() => {
-      this.imageSrc =
-        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1540037609752&di=78150c346d1b4986802d7edf31338c89&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F01cb11599aaeea0000002129536e52.gif';
-    }, 1000);
-  }
+  imageSrc = '/assets/logo.png';
 }
 </script>
 
